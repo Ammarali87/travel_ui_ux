91f62e8f85863acee7c6ba688e1105385064771b
Ammarali87 <amarhero501@gmail.com>
2024-12-06 08:54:11 -0800

    ggg

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,7 +14,7 @@ const Hero = () => {
           height={50}
           className="absolute left-[-5px] top-[-30px] w-10 lg:w-[50px]"
         />
-        <h1 className="bold-52 lg:bold-88">Putuk Truno Camp Area</h1>
+        <h1 className="bold-52 lg:bold-88">Sharm Elshaikh Area</h1>
         <p className="regular-16 mt-6 text-gray-30 xl:max-w-[520px]">
           We want to be on each of your journeys seeking the satisfaction of seeing the incorruptible beauty of nature. We can help you on an adventure around the world in just one app
         </p>
@@ -52,16 +52,29 @@ const Hero = () => {
           />
         </div>
       </div>
+  {/* flex-1  */}  
+           <div className="bg-red-900 flexBetween bold-20 relative felx flex-1 flex-col justify-center gap-12 max-w-[600px] p-1 ">
+           <div className="space-x-3 dark ">
+            <div className=" bg-blue-400 absolute p-3  hover:bg-green-900 z-40  -top-7 -right-7 max-md:mx-4">
+            <p className='inline-block'>helo world</p>
+              <Image className='inline-block' src="/close.svg" alt="close" width={24} height={24} />
+            </div>
+           </div>
+            <div className="">
 
+            <p className='inline-bloc '>helo world</p>
+              <Image className='inline-block' src="/close.svg" alt="close" width={24} height={24} />
+            </div>
+           </div>
       <div className="relative flex flex-1 items-start">
         <div className="relative z-20 flex w-[268px] flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
-
+             
            <div className="flex flex-col">
             <div className="flexBetween">
               <p className="regular-16 text-gray-20">Location</p>
               <Image src="/close.svg" alt="close" width={24} height={24} />
             </div>
-            <p className="bold-20 text-white">Aguas Calientes</p>
+            <p className="bold-20 text-white">Egypt </p>
           </div>
 
           <div className="flexBetween">
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,7 @@
-"use client"
+"use client";
+
 import { useState } from "react";
+import { usePathname } from "next/navigation";
 import { NAV_LINKS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
@@ -7,40 +9,48 @@ import Button from "./Button";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-    
+  const pathname = usePathname(); // Get the current path
+
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
         <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
       </Link>
 
-      <ul className={` flex flex-row  gap-12 max-md:hidden `}>
+      <ul className="flex flex-row gap-12 max-md:hidden">
         {NAV_LINKS.map((link) => (
-          <Link  
-            href={link.href} 
-            key={link.key} 
-            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
-            onClick={() => setIsMenuOpen(false)} // Close menu on link click
-          >
-            {link.label} 
-          </Link>
+          <li key={link.key}>
+            <Link
+              href={link.href}
+              className={`regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all ${
+                pathname === link.href ? "font-bold border-b-2 border-green-800" : "hover:bg-green-400 p-1.5 rounded-xl"
+              }`}   
+              onClick={() => setIsMenuOpen(false)}
+            >
+              {link.label}
+            </Link>
+          </li>
         ))}
       </ul>
-      <ul className={` h-full gap-12 lg:flex ${isMenuOpen ? "block" : "hidden"}`}>
+
+      <ul className={`h-full gap-12 lg:flex ${isMenuOpen ? "block" : "hidden"}`}>
         {NAV_LINKS.map((link) => (
-          <Link  
-            href={link.href} 
-            key={link.key} 
-            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
-            onClick={() => setIsMenuOpen(false)} // Close menu on link click
-          >
-            {link.label} 
-          </Link>
+          <li key={link.key}>
+            <Link
+              href={link.href}
+              className={`regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all ${
+                pathname === link.href ? "font-bold border-b-2 border-white" : "hover:font-bold"
+              }`}
+              onClick={() => setIsMenuOpen(false)}
+            >
+              {link.label}
+            </Link>
+          </li>
         ))}
       </ul>
 
       <div className="lg:flexCenter hidden">
-        <Button 
+        <Button
           type="button"
           title="Login"
           icon="/user.svg"
@@ -48,18 +58,18 @@ const Navbar = () => {
         />
       </div>
 
-      <Image   
+      <Image
         src={isMenuOpen ? "/menu.svg" : "/menu.svg"}
         alt="menu"
         width={32}
         height={32}
         className="inline-block cursor-pointer lg:hidden"
-        onClick={() => setIsMenuOpen((prev) => !prev)} // Toggle menu
+        onClick={() => setIsMenuOpen((prev) => !prev)}
         aria-label="Toggle menu"
         aria-expanded={isMenuOpen}
       />
     </nav>
   );
-}
+};
 
 export default Navbar;
